354d3a7ede82ba103e015f4876e234f6fe044a17
Luis Rocha <lerocha@gmail.com>
2017-02-26 20:51:00 -0800

    Making /currencies/available a hipermedia-based RESTful API

--- src/main/java/com/github/lerocha/currency/controller/CurrencyController.java
+++ src/main/java/com/github/lerocha/currency/controller/CurrencyController.java
@@ -3,13 +3,17 @@ package com.github.lerocha.currency.controller;
 import com.github.lerocha.currency.domain.Currency;
 import com.github.lerocha.currency.service.ExchangeRateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Locale;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by lerocha on 2/24/17.
@@ -24,8 +28,9 @@ public class CurrencyController {
         this.exchangeRateService = exchangeRateService;
     }
 
-    @RequestMapping(path = "/currencies/available")
-    public ResponseEntity<List<Currency>> getAvailableCurrencies(@RequestParam(name = "locale", required = false, defaultValue = "en_US") Locale locale) {
-        return ResponseEntity.ok(exchangeRateService.getAvailableCurrencies(locale));
+    @GetMapping(path = "/currencies/available")
+    public ResponseEntity<Resources<Currency>> getAvailableCurrencies(@RequestParam(name = "locale", required = false, defaultValue = "en_US") Locale locale) {
+        List<Currency> currencies = exchangeRateService.getAvailableCurrencies(locale);
+        return ResponseEntity.ok(new Resources<>(currencies, linkTo(methodOn(CurrencyController.class).getAvailableCurrencies(locale)).withSelfRel()));
     }
 }
