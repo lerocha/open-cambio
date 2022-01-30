7331eafe61528b67c65bb3959d555e2aa994fbe4
Luis Rocha <lerocha@gmail.com>
2022-01-30 12:55:17 -0800

    Logging time to complete the request.

--- src/main/java/com/github/lerocha/opencambio/filter/LoggerFilter.java
+++ src/main/java/com/github/lerocha/opencambio/filter/LoggerFilter.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import java.io.IOException;
-import java.util.Arrays;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -14,21 +16,27 @@ import java.util.stream.Collectors;
 public class LoggerFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        Instant start = Instant.now();
         String requestDetails = getRequestDetails(request);
         log.info("request; {}", requestDetails);
 
         chain.doFilter(request, response);
 
-        log.info("response; {}; time={}", requestDetails, 0);
+        Instant stop = Instant.now();
+        log.info("response; {}; time={}", requestDetails, Duration.between(start, stop).toMillis());
     }
 
     private String getRequestDetails(ServletRequest request) {
         if (request instanceof RequestFacade) {
             RequestFacade requestFacade = (RequestFacade) request;
-            String params = requestFacade.getParameterMap().entrySet().stream()
-                    .map(x -> String.format("%s=%s", x.getKey(), Arrays.toString(x.getValue())))
+            Map<String, String> map = new LinkedHashMap<>();
+            map.put("method", requestFacade.getMethod());
+            map.put("uri", requestFacade.getRequestURI());
+            requestFacade.getParameterMap().forEach((key, value) -> map.put(key, Arrays.toString(value)));
+
+            return map.entrySet().stream()
+                    .map(x -> String.format("%s=%s", x.getKey(), x.getValue()))
                     .collect(Collectors.joining("; "));
-            return String.format("method=%s; uri=%s; %s", requestFacade.getMethod(), requestFacade.getRequestURI(), params);
         }
         return "";
     }
