be175d3d8943fa1cae06ba3a2e11563543478703
Luis Rocha <lerocha@gmail.com>
2024-05-26 11:06:19 -0700

    chore: upgrading to Java 21

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM amazoncorretto:17
+FROM amazoncorretto:21-alpine
 
 RUN mkdir /opt/open-cambio
 COPY build/libs/open-cambio-0.0.2-SNAPSHOT.jar /opt/open-cambio/open-cambio-0.0.2-SNAPSHOT.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.github.lerocha"
 version = "0.0.2-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 configurations {
     compileOnly {
