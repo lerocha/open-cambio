542045d579179497a34eb072b8e045b8855674bb
Luis Rocha <lerocha@gmail.com>
2024-05-26 10:00:50 -0700

    chore: updating copyright

--- src/main/java/com/github/lerocha/opencambio/domain/Currency.java
+++ src/main/java/com/github/lerocha/opencambio/domain/Currency.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 Luis Rocha
+ * Copyright 2017-2024 Luis Rocha
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
