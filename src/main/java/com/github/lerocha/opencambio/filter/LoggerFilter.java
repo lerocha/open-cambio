67440862587bafb2d28891041f19643bf6cad908
Luis Rocha <lerocha@gmail.com>
2024-09-29 22:21:46 -0700

    feat: logging improvements (#16)

--- src/main/java/com/github/lerocha/opencambio/filter/LoggerFilter.java
+++ src/main/java/com/github/lerocha/opencambio/filter/LoggerFilter.java
@@ -24,6 +24,7 @@ import jakarta.servlet.ServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.RequestFacade;
 import org.apache.catalina.connector.ResponseFacade;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -32,52 +33,49 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class LoggerFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        Instant start = Instant.now();
-        String requestDetails = getRequestDetails(request);
-        log.info("request; {}", requestDetails);
-
-        chain.doFilter(request, response);
-
-        Instant stop = Instant.now();
-        String responseDetails = getResponseDetails(response);
-        log.info("response; {}; time={}; {}", responseDetails, Duration.between(start, stop).toMillis(), requestDetails);
+        var mdcEntries = getMdcEntries(request);
+        try {
+            Instant start = Instant.now();
+            mdcEntries.forEach(MDC::put);
+            chain.doFilter(request, response);
+            Instant stop = Instant.now();
+            log.info(getMessage(request, response, start, stop));
+        } finally {
+            mdcEntries.keySet().forEach(MDC::remove);
+        }
     }
 
-    private String getRequestDetails(ServletRequest request) {
+    private Map<String, String> getMdcEntries(ServletRequest request) {
+        Map<String, String> map = new LinkedHashMap<>();
         if (request instanceof RequestFacade requestFacade) {
-            Map<String, String> map = new LinkedHashMap<>();
             map.put("method", requestFacade.getMethod());
             map.put("uri", requestFacade.getRequestURI());
             requestFacade.getParameterMap().forEach((key, value) -> map.put(key, Arrays.toString(value)));
             String forwardedFor = requestFacade.getHeader("X-FORWARDED-FOR");
             String ipAddress = forwardedFor != null ? forwardedFor.split(",")[0] : requestFacade.getRemoteAddr();
-            map.put("ipAddress", ipAddress);
-            map.put("userAgent", requestFacade.getHeader("User-Agent"));
-            return mapToString(map);
+            map.put("ip", ipAddress);
+            map.put("agent", requestFacade.getHeader("User-Agent"));
         }
-        return "";
+        return map;
     }
 
-    private String getResponseDetails(ServletResponse response) {
+    private String getMessage(ServletRequest request, ServletResponse response, Instant start, Instant stop) {
+        StringBuilder builder = new StringBuilder();
+        if (request instanceof RequestFacade requestFacade) {
+            builder.append(requestFacade.getMethod()).append(" ")
+                    .append(requestFacade.getRequestURI()).append(", ");
+        }
         if (response instanceof ResponseFacade responseFacade) {
-            Map<String, String> map = new LinkedHashMap<>();
-            map.put("status", String.valueOf(responseFacade.getStatus()));
-            map.put("content-type", responseFacade.getContentType());
-            return mapToString(map);
+            builder.append("status=").append(responseFacade.getStatus()).append(", ")
+                    .append("content-type=").append(responseFacade.getContentType()).append(", ");
         }
-        return "";
-    }
-
-    private String mapToString(Map<String, String> map) {
-        return map.entrySet().stream()
-                .map(x -> String.format("%s=%s", x.getKey(), x.getValue()))
-                .collect(Collectors.joining("; "));
+        builder.append("time=").append(Duration.between(start, stop).toMillis()).append(",");
+        return builder.toString();
     }
 }
